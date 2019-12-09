25f56af2c9d6517448326e80b814b83d713a6af0
nikos <nikos@evolution>
2019-12-09 15:23:45 +0200

    deleted the test class

--- CardMatchingGame/src/CardMatchingGame.java
+++ CardMatchingGame/src/CardMatchingGame.java
@@ -3,7 +3,6 @@ public class CardMatchingGame {
 
 	public static void main(String[] args) {
 		System.out.print("Hello world from vaaldis");
-		Test.dosomething();
 
 	}
 
--- CardMatchingGame/src/Test.java
+++
@@ -1,7 +0,0 @@
-
-public class Test {
-	public static void dosomething() {
-		System.out.println("this is a new class");
-
-	}
-}
