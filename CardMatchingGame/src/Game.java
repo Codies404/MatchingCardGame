6308ee6f04d7bae97473d7a7fcce58a00941bcfc
BalP-AI <58683767+BalP-AI@users.noreply.github.com>
2019-12-09 17:58:06 +0200

    game menu finished stage 1

--- CardMatchingGame/src/Game.java
+++ CardMatchingGame/src/Game.java
@@ -1,41 +1,62 @@
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Game extends JFrame {
 
 	public Game() {
-		
+
 		JFrame jf = new JFrame("Shape Master!");
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JLabel label = new JLabel("New Game", JLabel.CENTER);
 		jf.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		// jf.setUndecorated(true);
-				jf.add(label, BorderLayout.NORTH);
-				label.setVisible(true);
-				
-				
-				
-				
-				JButton Easy=new JButton("Easy 5x5");
-				JButton Normal=new JButton("Normal 8x8");
-				JButton Hard=new JButton("Hard 10x10");
-				Container pane=getContentPane();
-				 
-				 FlowLayout flo=new FlowLayout();
-				
-				 pane.setLayout(flo);
-				
-				 pane.add(Easy);
-				 pane.add(Normal);
-				 pane.add(Hard);
-				
-				 jf.add(pane);
+		jf.add(label, BorderLayout.NORTH);
+		label.setVisible(true);
+		JPanel row1 = new JPanel();
+		JPanel row2 = new JPanel();
+		JPanel row3 = new JPanel();
+		JPanel row4 = new JPanel();
+		JButton Easy = new JButton("Easy");
+		JButton Normal = new JButton("Normal");
+		JButton Hard = new JButton("Hard");
+		JButton Back = new JButton("Back");
+		JTextField name = new JTextField(50);
+		Container pane = getContentPane();
+		JLabel username = new JLabel("Enter Your name.", JLabel.NORTH_EAST);
+		JLabel difficulty = new JLabel("Please choose difficulty.");
+		// JPanel pane1
+
+		GridLayout layout = new GridLayout(20, 1);
+		FlowLayout flowlayout = new FlowLayout();
+		pane.setLayout(layout);
+		row1.setLayout(flowlayout);
+		row1.add(username);
+		row1.add(name);
+
+		row2.add(difficulty);
+		row3.add(Easy);
+		row3.add(Normal);
+		row3.add(Hard);
+
+		row4.add(Back);
+
+		pane.add(row1);
+		pane.add(row2);
+		pane.add(row3);
+		pane.add(row4);
+		// pane.add(difficulty);
+
+		jf.add(pane);
 		jf.setVisible(true);
+
 	}
 
 }
