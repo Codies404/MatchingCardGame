03d4b806c2d423d7d2cce2c24c5b80e7c201fbf9
nikos <nikos@evolution>
2019-12-09 14:45:13 +0200

    add a new class

--- CardMatchingGame/src/CardMatchingGame.java
+++ CardMatchingGame/src/CardMatchingGame.java
@@ -3,6 +3,7 @@ public class CardMatchingGame {
 
 	public static void main(String[] args) {
 		System.out.print("Hello world from vaaldis");
+		Test.dosomething();
 
 	}
 
---
+++ CardMatchingGame/src/Test.java
@@ -0,0 +1,7 @@
+
+public class Test {
+	public static void dosomething() {
+		System.out.println("this is a new class");
+
+	}
+}
