c13667eba8e6b7ab79596fb40db9a0df3a980f64
nikos <nikos@evolution>
2019-12-09 16:40:25 +0200

    add menu

--- CardMatchingGame/src/CardMatchingGame.java
+++ CardMatchingGame/src/CardMatchingGame.java
@@ -1,12 +1,9 @@
-import javax.swing.JFrame;
 
 public class CardMatchingGame {
 
+	private final static int WIDTH = 1080 , HEIGHT = (WIDTH * 9)/16;	//the size of the window will have a resolution of 16:9 ratio 
 	public static void main(String[] args) {
-		JFrame jf = new JFrame();
-		jf.setExtendedState(JFrame.MAXIMIZED_BOTH); 
-		jf.setUndecorated(true);
-		jf.setVisible(true);
+		new Menu("Shape Master",WIDTH,HEIGHT);
 
 	}
 
